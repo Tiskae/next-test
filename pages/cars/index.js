346692caf83b82dda54ebb89a9e589fdc51f86a7
user <user@users-MacBook-Pro.local>
2022-07-16 18:50:51 +0100

    Practiced styling in next.js

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,3 +1,17 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
 export default function () {
-  return <h1>Cars list</h1>;
+  return (
+    <Layout>
+      <Head>
+        <title>Cars list page</title>
+      </Head>
+      <h1>Cars list</h1>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </Layout>
+  );
 }
